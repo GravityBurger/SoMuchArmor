6c3b03ba2e50fb708819af74910a99179f0ab2a2
GravityBurger <davidvestjr@sbcglobal.net>
2014-03-10 14:19:40 -0600

    Added Titanium Armor

--- GravityBurger/SoMuchArmor/common/SoMuchArmor.java
+++ GravityBurger/SoMuchArmor/common/SoMuchArmor.java
@@ -17,6 +17,7 @@ import GravityBurger.SoMuchArmor.item.ItemTitaniumIngot;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.registry.GameRegistry;
+import GravityBurger.SoMuchArmor.armor.TitaniumArmor;
 
 
 @Mod(modid = "SoMuchArmor", name = "So Much Armor", version = "Alpha 1.0")
@@ -27,16 +28,27 @@ public static CommonProxy proxy;
 	
 //Armor ID's
 private static int NightVision;
+private static int TitaniumHat;
+private static int TitaniumShirt;
+private static int TitaniumPants;
+private static int TitaniumShoes;
+
+
 
 //Armor Material
 public static ArmorMaterial NightVisionArmor = EnumHelper.addArmorMaterial("NIGHTVISION", 15, new int[] {2, 0, 0, 0}, 10);
-
+public static ArmorMaterial TitaniumArmor = EnumHelper.addArmorMaterial("TITANIUM", 42, new int[] {4, 8, 6, 2}, 5);
 
 //Armor Registration
 
+//Night Vision
 public static Item NightVisionHelmet = new NightVisionArmor(NightVisionArmor, NightVision, 0).setUnlocalizedName("NightVisionHelmet").setTextureName("SoMuchArmor:NightVisionHelmet");
 
-
+//Titanium
+public static Item TitaniumHelmet = new TitaniumArmor(TitaniumArmor, TitaniumHat, 0).setUnlocalizedName("TitaniumHelmet").setTextureName("SoMuchArmor:TitaniumHelmet");
+public static Item TitaniumChestplate = new TitaniumArmor(TitaniumArmor, TitaniumShirt, 1).setUnlocalizedName("TitaniumChestplate").setTextureName("SoMuchArmor:TitaniumChestplate");
+public static Item TitaniumLeggings = new TitaniumArmor(TitaniumArmor, TitaniumPants, 2).setUnlocalizedName("TitaniumLeggings").setTextureName("SoMuchArmor:TitaniumLeggings");
+public static Item TitaniumBoots = new TitaniumArmor(TitaniumArmor, TitaniumShoes, 3).setUnlocalizedName("TitaniumBoots").setTextureName("SoMuchArmor:TitaniumBoots");
 //WorldGen
 
 public static TitaniumOreWG worldgen1 = new TitaniumOreWG();
@@ -62,6 +74,11 @@ public static Block TitaniumOre = new BlockTitaniumOre(3000, Material.rock).setB
 		
 		//Armor GameRegistry
 		GameRegistry.registerItem(NightVisionHelmet, "NightVisionHelmet");
+		GameRegistry.registerItem(TitaniumHelmet, "TitaniumHelmet");
+		GameRegistry.registerItem(TitaniumChestplate, "TitaniumChestplate");
+		GameRegistry.registerItem(TitaniumLeggings, "TitaniumLeggings");
+		GameRegistry.registerItem(TitaniumBoots, "TitaniumBoots");
+		
 		
 		
 		
@@ -79,13 +96,11 @@ public static Block TitaniumOre = new BlockTitaniumOre(3000, Material.rock).setB
 		
 		//Armor Recipes
 		
-		
-		
-		
-		
 		GameRegistry.addShapedRecipe(new ItemStack(NightVisionHelmet, 1), "XXX", "XOX", "  ", 'X', SteelIngot, 'O', Goggles);
-		GameRegistry.addShapedRecipe(new ItemStack(Goggles, 1), "  ", "X0X", "  ", 'X', Blocks.glass, '0', Blocks.redstone_block);
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TitaniumHelmet, 1), "XXX", "X X", "  ", 'X', TitaniumIngot);
+		GameRegistry.addShapedRecipe(new ItemStack(TitaniumChestplate, 1), "X X", "XXX", "XXX", 'X', TitaniumIngot);
+		GameRegistry.addShapedRecipe(new ItemStack(TitaniumLeggings, 1), "XXX", "X X", "X X", 'X', TitaniumIngot);
+		GameRegistry.addShapedRecipe(new ItemStack(TitaniumBoots, 1), "   ", "X X", "X X", 'X', TitaniumIngot);
 		
 		
 		
@@ -94,6 +109,7 @@ public static Block TitaniumOre = new BlockTitaniumOre(3000, Material.rock).setB
 		
 		GameRegistry.addSmelting(Items.iron_ingot, new ItemStack(SteelIngot, 1), 10);
 		GameRegistry.addSmelting(TitaniumOre, new ItemStack(TitaniumIngot, 1), 10);
+		GameRegistry.addShapedRecipe(new ItemStack(Goggles, 1), "  ", "X0X", "  ", 'X', Blocks.glass, '0', Blocks.redstone_block);
 		
 		
 		
