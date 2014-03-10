fd6528353158618dbf1e4ad1f1c0f829e3ebdf4b
GravityBurger <davidvestjr@sbcglobal.net>
2014-03-09 22:18:27 -0600

    Setting up the whole forge thing!

---
+++ GravityBurger/SoMuchArmor/common/SoMuchArmor.java
@@ -0,0 +1,9 @@
+package GravityBurger.SoMuchArmor.common;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "SoMuchArmor", name = "So Much Armor", version = "Alpha 1.0")
+
+public class SoMuchArmor {
+
+}
